0abc158ab9d3fb18e252eca96dcd97a43725ab62
Adrián Kóša <adrian.kosa@slovenskoit.sk>
2022-03-28 09:55:21 +0200

    WIP add card, meals, mealItem components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Header from "./components/Layout/Header";
+import { Fragment } from "react";
+import Meals from "./components/Meals/Meals";
+
 function App() {
   return (
-    <div>
-      <h2>WIP - lorem ipsum...</h2>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
